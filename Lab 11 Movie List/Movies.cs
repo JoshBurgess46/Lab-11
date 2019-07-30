c7f2b1d464614c037c2b532bb4991e28cc52d4c8
Josh Burgess <JoshBurgess_46@outlook.com>
2019-07-30 09:18:15 -0400

    lab11 movies

---
+++ Lab 11 Movie List/Movies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_11_Movie_List
+{
+    public class Movies
+    {
+        
+        public string title { get; set; }
+        public string category { get; set; }
+
+        public Movies(string title, string category)
+        {
+            this.title = title;
+            this.category = category;
+        }
+        
+    }
+
+}
---
+++ Lab 11 Movie List/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_11_Movie_List
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\tWelcome to Grand Circus Movies! Hit enter to see our categories.\t");
+            List<Movies> movieListings = new List<Movies>
+                    {
+                        new Movies("The Shining", "horror"),
+                        new Movies("Paranormal Activity","horror"),
+                        new Movies("The Conjuring", "horror"),
+                        new Movies("Schindler's List","drama"),
+                        new Movies("The Help","drama"),
+                        new Movies("The Godfather","drama"),
+                        new Movies("The Matrix","scifi"),
+                        new Movies("Alien","scifi"),
+                        new Movies("Interstellar","scifi"),
+                        new Movies("Big Hero 6","animated"),
+                        new Movies("Inside Out","animated"),
+                        new Movies("Toy Story","animated")
+                    };
+            bool go = true;
+            while (go)
+            {
+
+
+
+
+                
+
+                DisplayMenu(movieListings);
+
+
+
+
+            }
+
+
+        }
+
+        public static void DisplayMenu(List<Movies> movieList)
+        {
+            Console.WriteLine("\tPick a genre.\t");
+            Console.WriteLine();
+            Console.WriteLine("\thorror\t");
+            Console.WriteLine();
+            Console.WriteLine("\tdrama\t");
+            Console.WriteLine();
+            Console.WriteLine("\tscifi\t");
+            Console.WriteLine();
+            Console.WriteLine("\tanimated\t");
+            Console.WriteLine();
+            Console.WriteLine("\tquit\t");
+            Console.WriteLine();
+
+
+            string userInput = Console.ReadLine().ToLower();
+
+            foreach (Movies movie in movieList)
+            {
+
+                if (movie.category == userInput)
+                {
+                    Console.WriteLine(movie.title);
+
+                }
+                
+                
+                
+            }
+            if (userInput == "quit")
+            {
+                Console.WriteLine("Stay golden pony boy");
+                System.Environment.Exit(0);
+            }
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+        }
+    }
+}
